ae7ec9208a230566d14e1a5c5b23339ad64c1edc
potatoesAreGod <118043038+potatoesAreGod@users.noreply.github.com>
2023-05-31 18:48:05 +0200

    added ability to save images

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from src.scan import scan_document
 from src.save import save_document
+from os import path
 
 debug = 1  # enables debugging output
 demo = 0  # auto-loads a demo image
 demo_img = "./images/demo.png"  # image to be used in the demo
-filetype = "pdf"  # format to save as; pdf, png. leave empty to not save
+filetype = ".png"  # format to save as. leave empty to not save
+save_path = path.join(path.expanduser("~"), "Downloads")  # where to save scanned docs. defaults to user's downloads
 
 # TODO: Make use to the DEBUG var
 # TODO: OCR -> Editable document
-# TODO: Save to PDF
-# TODO: Save to PNG
 # TODO: Create a UI for dropping images (Web/Desktop/React(or is it electron?) -> convert to web+desktop)
 
 if __name__ == "__main__":
@@ -18,4 +18,4 @@ if __name__ == "__main__":
     else:
         doc = scan_document("./images/demo.png", debug)
 
-    save_document(doc, filetype)
+    save_document("document", filetype, save_path, doc)
--- src/save.py
+++ src/save.py
@@ -1,3 +1,31 @@
-def save_document(document, filetype):
-    if filetype == "":
+import cv2
+from os import remove, path
+from PIL import Image
+
+
+def save_document(filename, filetype, save_path, image):
+    if filetype in [".jpeg", ".png", ".pxm", ".exr", ".tiff", ".jpeg2000", ".pam"]:
+        image_name = path.join(save_path, filename + filetype)
+        # save the image
+        cv2.imwrite(image_name, image)
+
+    elif filetype == "pdf":
+        image_name = path.join(save_path, filename + ".png")
+        # save the image
+        cv2.imwrite(image_name, image)
+        # set filename
+        pdf_name = filename + ".pdf"
+        image_path = path.join(save_path, image_name)
+        pdf_path = path.join(save_path, pdf_name)
+        # open the previously saved image
+        image = Image.open(image_path)
+        # convert
+        im = image.convert("RGB")
+        # save as pdf
+        im.save(pdf_path)
+        # remove unwanted image; we only want the pdf
+        remove(image_path)
+
+    else:
+        print("Not saving document!")
         return
